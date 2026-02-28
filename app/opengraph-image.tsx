faa29871db7a722a6aacb06c7b4d5031f00f9c9f
cmass0 <mnz1223@naver.com>
2026-02-28 19:03:18 +0900

    랜딩 페이지 section1 height 크기 조절 & og:image, twitter-image 메타 추가 & buford 신규 공장 location 추가

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,9 @@
+import { renderSocialImage, socialImageSize } from "@/lib/social-image";
+
+export const alt = "Wellins Inc. social preview";
+export const size = socialImageSize;
+export const contentType = "image/png";
+
+export default async function OpenGraphImage() {
+  return renderSocialImage();
+}
